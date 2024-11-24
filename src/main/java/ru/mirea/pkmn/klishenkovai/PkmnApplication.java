516821d1e816f65e40cf632f0e2969d1fa770ad0
artem <artem.klishenkov@gmail.com>
2024-11-24 22:45:53 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/pkmn/klishenkovai/PkmnApplication.java
@@ -0,0 +1,47 @@
+package ru.mirea.pkmn.klishenkovai;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import ru.mirea.pkmn.*;
+import ru.mirea.pkmn.klishenkovai.web.http.PkmnHttpClient;
+import ru.mirea.pkmn.klishenkovai.web.jdbc.DatabaseServiceImpl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class PkmnApplication {
+
+    public static void main(String[] args) throws IOException, SQLException {
+        CardImport pokemon = new CardImport("my_card.txt");
+
+        PkmnHttpClient pkmnHttpClient = new PkmnHttpClient();
+
+        JsonNode json = pkmnHttpClient.getPokemonCard(pokemon.card.getName(), pokemon.card.getNumber());
+        //System.out.println(card.toPrettyString());
+
+        //doDescription(pokemon.card, json);
+        CardExport cardExport = new CardExport(pokemon.card);
+        System.out.println(pokemon.card.toString());
+
+        DatabaseServiceImpl databaseService = new DatabaseServiceImpl();
+        databaseService.createPokemonOwner(pokemon.card.getPokemonOwner());
+        databaseService.saveCardToDatabase(pokemon.card);
+        Student student = databaseService.getStudentFromDatabase("Клишенков Артём Игоревич");
+        Card card = databaseService.getCardFromDatabase("Landorus");
+        System.out.println(card.toString());
+
+        System.exit(0);
+    }
+    public static void doDescription(Card card, JsonNode main_node) {
+        for (JsonNode node : main_node.path("data")) {
+            if (node.findValue("id").asText().equals("sv105")) {
+                main_node = node.path("attacks");
+                break;
+            }
+        }
+        int i = 0;
+        for (AttackSkill attack : card.getSkills()) {
+            attack.setDescription(main_node.get(i).findValue("text").asText());
+            i++;
+        }
+    }
+}
